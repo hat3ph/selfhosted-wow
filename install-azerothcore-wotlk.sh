c669a5686242245b964457df97002447a7b87a4d
hat3ph <88069788+hat3ph@users.noreply.github.com>
2026-08-04 13:19:03 +0800

    set mysql root password

--- install-azerothcore-wotlk.sh
+++ install-azerothcore-wotlk.sh
@@ -11,7 +11,15 @@ cat << "EOF"
 https://www.azerothcore.org
 EOF
 
-function install(){
+function install
+	# set default env variable
+	AC_CODE_DIR="/opt/azerothcore-wotlk"
+	DB_USER="acore"
+	DB_PASS="P@ssw0rd123"
+	INSTALL_USER=$(whoami)
+	REALMLIST_IP=$(hostname -I | awk '{print $1}')
+	REALMLIST_NAME="AzerothCore WotLK"
+
 	# install dependencies
 	echo -e
 	echo "###########################################"
@@ -35,15 +43,8 @@ function install(){
 	echo "###########################"
 	echo "#...Secure MySQL server...#"
 	echo "###########################"
-	sudo mysql_secure_installation --use-default
-
-	# set default env variable
-	AC_CODE_DIR="/opt/azerothcore-wotlk"
-	DB_USER="acore"
-	DB_PASS="P@ssw0rd123"
-	INSTALL_USER=$(whoami)
-	REALMLIST_IP=$(hostname -I | awk '{print $1}')
-	REALMLIST_NAME="AzerothCore WotLK"
+	sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_PASS}';"
+	sudo mysql_secure_installation -u root --password=${DB_PASS} --use-default
 
 	if [[ -d ${AC_CODE_DIR} ]]; then
 		echo -e
