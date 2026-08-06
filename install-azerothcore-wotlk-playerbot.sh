95f20a97cbf091471b836d60470472c62959e8ac
hat3ph <88069788+hat3ph@users.noreply.github.com>
2026-08-06 10:50:48 +0800

    fix libstdc and jemalloc compile error

--- install-azerothcore-wotlk-playerbot.sh
+++ install-azerothcore-wotlk-playerbot.sh
@@ -82,6 +82,13 @@ function install(){
 		echo "${LATEST_CLIENT}" > ${AC_CODE_DIR}/data/.version
 		echo "Done extract client data to ${AC_CODE_DIR}/data."
 
+		if [[ $CODENAME == "resolute" ]]; then
+			# fix compile error missing libstdc++ library on Ubuntu 26.04 LTS
+			sudo apt-get install -y libstdc++-16-dev
+			# fix jemalloc compile error (https://github.com/389ds/389-ds-base/issues/7178)
+			sed -i 's|std::__throw_bad_alloc();|throw std::bad_alloc();|' ${AC_CODE_DIR}/deps/jemalloc/src/jemalloc_cpp.cpp
+		fi
+
 		# start compiling AzerothCore
 		echo -e
 		echo "#############################"
